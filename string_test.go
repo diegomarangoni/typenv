76ad151dad0e5933e7d8217ff55ecb628e409429
Diego Marangoni <diegomarangoni@me.com>
2025-04-02 23:15:11 +0200

    remove unused code

--- string_test.go
+++ string_test.go
@@ -3,7 +3,6 @@ package typenv_test
 import (
 	"os"
 	"testing"
-	"time"
 
 	"diegomarangoni.dev/typenv"
 	"github.com/stretchr/testify/assert"
@@ -54,28 +53,4 @@ func TestString(t *testing.T) {
 		got := typenv.String("TEST_STRING_SET_WITH_DEFAULT", "something else")
 		assert.Equal(t, expected, got)
 	})
-}
-
-func init() {
-	var _ time.Duration = typenv.Duration("MY_ENV")
-	var _ string = typenv.String("MY_ENV")
-	var _ float64 = typenv.Float64("MY_ENV")
-	var _ float32 = typenv.Float32("MY_ENV")
-	var _ int64 = typenv.Int64("MY_ENV")
-	var _ int32 = typenv.Int32("MY_ENV")
-	var _ int16 = typenv.Int16("MY_ENV")
-	var _ int8 = typenv.Int8("MY_ENV")
-	var _ int = typenv.Int("MY_ENV")
-	var _ bool = typenv.Bool("MY_ENV")
-
-	var _ []time.Duration = typenv.Slice[[]time.Duration]("MY_ENV", ",")
-	var _ []string = typenv.Slice[[]string]("MY_ENV", ",")
-	var _ []float64 = typenv.Slice[[]float64]("MY_ENV", ",")
-	var _ []float32 = typenv.Slice[[]float32]("MY_ENV", ",")
-	var _ []int64 = typenv.Slice[[]int64]("MY_ENV", ",")
-	var _ []int32 = typenv.Slice[[]int32]("MY_ENV", ",")
-	var _ []int16 = typenv.Slice[[]int16]("MY_ENV", ",")
-	var _ []int8 = typenv.Slice[[]int8]("MY_ENV", ",")
-	var _ []int = typenv.Slice[[]int]("MY_ENV", ",")
-	var _ []bool = typenv.Slice[[]bool]("MY_ENV", ",")
 }
